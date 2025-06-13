29b3809b63f9791273d2a06aa2403957d4e69e71
alekho <ahozin@hotmail.com>
2025-06-13 19:41:18 +0200

    copying application document files to the final folder

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'LICENSE.md',
+          dest: ''
+        },
+        {
+          src: 'README.md',
+          dest: ''
+        },
+        {
+          src: 'disclaimer.md',
+          dest: ''
+        },
+        {
+          src: 'privacy.md',
+          dest: ''
+        },
+        {
+          src: 'terms.md',
+          dest: ''
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
